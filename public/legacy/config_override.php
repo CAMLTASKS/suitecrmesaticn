59f094586a7e050cc6e46d7b064706c5633db23c
ALEJANDRO CAMLTASKS <c.a.m.l.123@hotmail.com>
2026-01-19 22:25:07 -0500

    Subida de ambiente

---
+++ public/legacy/config_override.php
@@ -0,0 +1,6 @@
+<?php
+/***CONFIGURATOR***/
+$sugar_config['default_export_charset'] = 'ISO-8859-1';
+$sugar_config['dbconfigoption']['collation'] = 'utf8mb3_bin';
+$sugar_config['default_language'] = 'es_MX';
+/***CONFIGURATOR***/
